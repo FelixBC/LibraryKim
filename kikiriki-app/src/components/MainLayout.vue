844961976a6f11cd180e5bf20abe516c3c10c2b3
FelixBC <Felixblancocabrera@gmail.com>
2023-06-06 10:19:33 -0400

    adding some style

--- kikiriki-app/src/components/MainLayout.vue
+++ kikiriki-app/src/components/MainLayout.vue
@@ -9,21 +9,77 @@
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Title
+          Kikiriki
         </q-toolbar-title>
       </q-toolbar>
 
       <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One"/>
-        <q-route-tab to="/page2" label="Page Two"/>
-        <q-route-tab to="/page3" label="Page Three"/>
+        <q-route-tab to="/page1" label="Registro"/>
+        <q-route-tab to="/page2" label="Informe"/>
+        <q-route-tab to="/page3" label="Preferidos"/>
       </q-tabs>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
-      Drawer content here.
-      <!-- drawer content -->
+    <q-drawer
+        v-model="drawer"
+        show-if-above
+
+        :mini="miniState"
+        @mouseover="miniState = false"
+        @mouseout="miniState = true"
+
+        :width="200"
+        :breakpoint="500"
+        bordered
+        :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+      <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }">
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox" class="custom-icon-color" />
+            </q-item-section>
+
+            <q-item-section>
+              Inbox
+            </q-item-section>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="star" color="blue"/>
+            </q-item-section>
+
+            <q-item-section>
+              Star
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="send" color="orange" />
+            </q-item-section>
+
+            <q-item-section>
+              Send
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="drafts" color="teal" />
+            </q-item-section>
+
+            <q-item-section>
+              Drafts
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
+
 
     <q-page-container>
       <router-view/>
@@ -31,12 +87,25 @@
 
   </q-layout>
 </template>
-
-<script setup>
+<style>
+.custom-icon-color {
+  color: rgb(120, 20, 66);
+}
+</style>
+<script>
 import {ref} from 'vue'
 
 const leftDrawerOpen = ref(false)
 const toggleLeftDrawer = () => {
   leftDrawerOpen.value = !leftDrawerOpen.value
+}
+
+export default {
+  setup () {
+    return {
+      drawer: ref(false),
+      miniState: ref(true)
+    }
+  }
 }
 </script>
