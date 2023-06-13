1c81ba047e9ba6d96e6febfc61a26f3eecb26def
FelixBC <Felixblancocabrera@gmail.com>
2023-06-13 10:58:38 -0400

    create works just fine

--- api/app/controllers/people_controller.rb
+++ api/app/controllers/people_controller.rb
@@ -18,7 +18,7 @@ class PeopleController < ApplicationController
     @person = Person.new(person_params)
 
     if @person.save
-      render json: @person, status: :created, location: @person
+      render json: @person, status: :created, location: people_url
     else
       render json: @person.errors, status: :unprocessable_entity
     end
