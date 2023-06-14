48b2ecbd9ae6ac755408e4f4271ac89a6a0bbc4c
FelixBC <Felixblancocabrera@gmail.com>
2023-06-14 17:23:17 -0400

    CRUD working smooth

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :people, only: [:create]
+  get '/people', to: 'people#index'
+  put '/people/:id', to: 'people#update'
+  delete '/people/:id', to: 'people#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
