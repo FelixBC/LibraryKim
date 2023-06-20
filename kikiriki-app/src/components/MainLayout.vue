e5baa9010247174854f374d6d64acbce3fc92672
FelixBC <Felixblancocabrera@gmail.com>
2023-06-19 21:36:39 -0400

    going back to quasar

--- kikiriki-app/src/components/MainLayout.vue
+++ kikiriki-app/src/components/MainLayout.vue
@@ -34,85 +34,66 @@
       </q-header>
 
 
-      <!--========== NAV ==========-->
-      <div class="nav" id="navbar">
-        <nav class="nav__container">
-          <div>
-            <a href="#" class="nav__link nav__logo">
-              <i class='bx bxs-disc nav__icon'></i>
-            </a>
+      <!--========== DRAWER ==========-->
+      <q-drawer
+          v-model="drawer"
+          show-if-above
 
-            <div class="nav__list">
-              <div class="nav__items">
-                <h3 class="nav__subtitle">Profile</h3>
+          :mini="miniState"
+          @mouseover="miniState = false"
+          @mouseout="miniState = true"
 
-                <a href="#" class="nav__link active">
-                  <i class='bx bx-home nav__icon'></i>
-                  <span class="nav__name">Home</span>
-                </a>
+          :width="200"
+          :breakpoint="500"
+          bordered
+          :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+      >
+        <q-scroll-area class="fit">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
 
-                <div class="nav__dropdown">
-                  <a href="#" class="nav__link">
-                    <i class='bx bx-user nav__icon'></i>
-                    <span class="nav__name">Users</span>
-                    <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
-                  </a>
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
 
-                  <div class="nav__dropdown-collapse">
-                    <div class="nav__dropdown-content">
-                      <a href="#" class="nav__dropdown-item">List</a>
-                      <a href="#" class="nav__dropdown-item">Create</a>
-                      <a href="#" class="nav__dropdown-item">Restrictions</a>
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
 
-                    </div>
-                  </div>
-                </div>
+              <q-item-section>
+                Star
+              </q-item-section>
+            </q-item>
 
-                <a href="#" class="nav__link">
-                  <i class='bx bx-message-rounded nav__icon'></i>
-                  <span class="nav__name">Messages</span>
-                </a>
-              </div>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
 
-              <div class="nav__items">
-                <h3 class="nav__subtitle">Menu</h3>
+              <q-item-section>
+                Send
+              </q-item-section>
+            </q-item>
 
-                <div class="nav__dropdown">
-                  <a href="#" class="nav__link">
-                    <i class='bx bx-bell nav__icon'></i>
-                    <span class="nav__name">Notifications</span>
-                    <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
-                  </a>
+            <q-separator />
 
-                  <div class="nav__dropdown-collapse">
-                    <div class="nav__dropdown-content">
-                      <a href="#" class="nav__dropdown-item">Blocked</a>
-                      <a href="#" class="nav__dropdown-item">Silenced</a>
-                      <a href="#" class="nav__dropdown-item">Publish</a>
-                      <a href="#" class="nav__dropdown-item">Program</a>
-                    </div>
-                  </div>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
 
-                </div>
-
-                <a href="#" class="nav__link">
-                  <i class='bx bx-compass nav__icon'></i>
-                  <span class="nav__name">Explore</span>
-                </a>
-                <a href="#" class="nav__link">
-                  <i class='bx bx-bookmark nav__icon'></i>
-                  <span class="nav__name">Saved</span>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <a href="#" class="nav__link nav__logout">
-            <i class='bx bx-log-out nav__icon'></i>
-            <span class="nav__name">Log Out</span>
-          </a>
-        </nav>
-      </div>
+              <q-item-section>
+                Drafts
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
 
       <q-page-container>
         <router-view/>
@@ -124,352 +105,6 @@
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap");
 
-/*========== VARIABLES CSS ==========*/
-:root {
-  --header-height: 3.5rem;
-  --nav-width: 219px;
-
-  /*========== Colors ==========*/
-  --first-color: #1976D2;
-  --first-color-light: #F4F0FA;
-  --title-color: #19181B;
-  --text-color: #58555E;
-  --text-color-light: #A5A1AA;
-  --body-color: #F9F6FD;
-  --container-color: #FFFFFF;
-
-  /*========== Font and typography ==========*/
-  --body-font: 'Poppins', sans-serif;
-  --normal-font-size: .938rem;
-  --small-font-size: .75rem;
-  --smaller-font-size: .75rem;
-
-  /*========== Font weight ==========*/
-  --font-medium: 500;
-  --font-semi-bold: 600;
-
-  /*========== z index ==========*/
-  --z-fixed: 100;
-}
-
-@media screen and (min-width: 1024px) {
-  :root {
-    --normal-font-size: 1rem;
-    --small-font-size: .875rem;
-    --smaller-font-size: .813rem;
-  }
-}
-
-/*========== BASE ==========*/
-*, ::before, ::after {
-  box-sizing: border-box;
-}
-
-body {
-  padding: calc(var(--header-height) + 1rem) 1rem 0;
-  margin: 0;
-  padding: 0;
-  padding-top: 60%;
-  font-family: var(--body-font);
-  font-size: var(--normal-font-size);
-  background-color: var(--body-color);
-  color: var(--text-color);
-}
-
-
-h3 {
-  margin: 0;
-}
-
-a {
-  text-decoration: none;
-}
-
-img {
-  padding-left: 10%;
-  max-width: 100%;
-  height: auto;
-}
-
-/*========== HEADER ==========*/
-.header {
-  margin-left: 0;
-  padding-left: 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  background-color: var(--container-color);
-  box-shadow: 0 1px 0 rgba(22, 8, 43, 0.1);
-  z-index: var(--z-fixed);
-}
-
-.header__container {
-  display: flex;
-  align-items: center;
-  height: var(--header-height);
-  justify-content: space-between;
-}
-
-.header__img {
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-}
-
-.header__logo {
-  color: var(--title-color);
-  font-weight: var(--font-medium);
-  display: none;
-}
-
-.header__search {
-  display: flex;
-  padding: .40rem .75rem;
-  background-color: var(--first-color-light);
-  border-radius: .25rem;
-}
-
-.header__input {
-  width: 100%;
-  border: none;
-  outline: none;
-  background-color: var(--first-color-light);
-}
-
-.header__input::placeholder {
-  font-family: var(--body-font);
-  color: var(--text-color);
-}
-
-.header__icon,
-.header__toggle {
-  font-size: 1.2rem;
-}
-
-.header__toggle {
-  color: var(--title-color);
-  cursor: pointer;
-}
-
-/*========== NAV ==========*/
-.nav {
-
-  position: fixed;
-  top: var(--header-height); /* Update this line */
-  height: 100vh;
-  padding: 1rem 1rem 0;
-  background-color: var(--container-color);
-  box-shadow: 1px 0 0 rgba(22, 8, 43, 0.1);
-  z-index: var(--z-fixed);
-  transition: .4s;
-}
-
-.nav__container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding-bottom: 3rem;
-  scrollbar-width: none; /* For mozilla */
-}
-
-/* For Google Chrome and others */
-.nav__container::-webkit-scrollbar {
-  display: none;
-}
-
-.nav__logo {
-  font-weight: var(--font-semi-bold);
-  margin-bottom: 2.5rem;
-}
-
-.nav__list,
-.nav__items {
-  display: grid;
-}
-
-.nav__list {
-  row-gap: 2.5rem;
-}
-
-.nav__items {
-  row-gap: 1.5rem;
-}
-
-.nav__subtitle {
-  font-size: var(--normal-font-size);
-  text-transform: uppercase;
-  letter-spacing: .1rem;
-  color: var(--text-color-light);
-}
-
-.nav__link {
-  display: flex;
-  align-items: center;
-  color: var(--text-color);
-}
-
-.nav__link:hover {
-  color: var(--first-color);
-}
-
-.nav__icon {
-  font-size: 1.2rem;
-  margin-right: .5rem;
-}
-
-.nav__name {
-  font-size: var(--small-font-size);
-  font-weight: var(--font-medium);
-  white-space: nowrap;
-}
-
-.nav__logout {
-  margin-top: 5rem;
-}
-
-/* Dropdown */
-.nav__dropdown {
-  overflow: hidden;
-  max-height: 21px;
-  transition: .4s ease-in-out;
-}
-
-.nav__dropdown-collapse {
-  background-color: var(--first-color-light);
-  border-radius: .25rem;
-  margin-top: 1rem;
-}
-
-.nav__dropdown-content {
-  display: grid;
-  row-gap: .5rem;
-  padding: .75rem 2.5rem .75rem 1.8rem;
-}
-
-.nav__dropdown-item {
-  font-size: var(--smaller-font-size);
-  font-weight: var(--font-medium);
-  color: var(--text-color);
-}
-
-.nav__dropdown-item:hover {
-  color: var(--first-color);
-}
-
-.nav__dropdown-icon {
-  margin-left: auto;
-  transition: .4s;
-}
-
-/* Show dropdown collapse */
-.nav__dropdown:hover {
-  max-height: 100rem;
-}
-
-/* Rotate icon arrow */
-.nav__dropdown:hover .nav__dropdown-icon {
-  transform: rotate(180deg);
-}
-
-/*===== Show menu =====*/
-.show-menu {
-  left: 0;
-}
-
-/*===== Active link =====*/
-.active {
-  color: var(--first-color);
-}
-
-
-/* ========== MEDIA QUERIES ==========*/
-/* For small devices reduce search*/
-@media screen and (max-width: 320px) {
-  .header__search {
-    width: 70%;
-  }
-}
-
-@media screen and (min-width: 768px) {
-  body {
-    padding: 1rem 3rem 0 6rem;
-  }
-
-  .header {
-    padding: 0 3rem 0 6rem;
-  }
-
-  .header__container {
-    height: calc(var(--header-height) + .5rem);
-  }
-
-  .header__search {
-    width: 300px;
-    padding: .55rem .75rem;
-  }
-
-  .header__toggle {
-    display: none;
-  }
-
-  .header__logo {
-    display: block;
-  }
-
-  .header__img {
-    width: 40px;
-    height: 40px;
-    order: 1;
-  }
-
-  .nav {
-    left: 0;
-    padding: 1.2rem 1.5rem 0;
-    width: 68px; /* Reduced navbar */
-  }
-
-  .nav__items {
-    row-gap: 1.7rem;
-  }
-
-  .nav__icon {
-    font-size: 1.3rem;
-  }
-
-  /* Element opacity */
-  .nav__logo-name,
-  .nav__name,
-  .nav__subtitle,
-  .nav__dropdown-icon {
-    opacity: 0;
-    transition: .3s;
-  }
-
-
-  /* Navbar expanded */
-  .nav:hover {
-    width: var(--nav-width);
-  }
-
-  /* Visible elements */
-  .nav:hover .nav__logo-name {
-    opacity: 1;
-  }
-
-  .nav:hover .nav__subtitle {
-    opacity: 1;
-  }
-
-  .nav:hover .nav__name {
-    opacity: 1;
-  }
-
-  .nav:hover .nav__dropdown-icon {
-    opacity: 1;
-  }
-}
 </style>
 <script lang="ts" setup>
 import {ref, Ref} from 'vue';
@@ -495,33 +130,5 @@ const gotoRegistration = (): void => {
 const toggleLeftDrawer = (): void => {
   drawer.value = !drawer.value;
 };
-
-/*==================== SHOW NAVBAR ====================*/
-const showMenu = (headerToggle: string, navbarId: string): void => {
-  const toggleBtn: HTMLElement | null = document.getElementById(headerToggle);
-  const nav: HTMLElement | null = document.getElementById(navbarId);
-
-  // Validate that variables exist
-  if (toggleBtn && nav) {
-    toggleBtn.addEventListener('click', (): void => {
-      // We add the show-menu class to the div tag with the nav__menu class
-      nav.classList.toggle('show-menu');
-      // change icon
-      toggleBtn.classList.toggle('bx-x');
-    });
-  }
-};
-
-showMenu('header-toggle', 'navbar');
-
-/*==================== LINK ACTIVE ====================*/
-const linkColor: NodeListOf<Element> = document.querySelectorAll('.nav__link');
-
-function colorLink(): void {
-  linkColor.forEach((l: Element): void => l.classList.remove('active'));
-  this.classList.add('active');
-}
-
-linkColor.forEach((l: Element): void => l.addEventListener('click', colorLink));
 
 </script>
